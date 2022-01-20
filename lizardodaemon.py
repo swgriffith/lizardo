2ae4dbb7b2b68bdf19beac0319099a08e646c709
Steve Griffith <stgriffi@microsoft.com>
2022-01-20 16:25:39 -0500

    added multiplexor

--- lizardodaemon.py
+++ lizardodaemon.py
@@ -37,11 +37,11 @@ def tca_select(channel):
         return
     tca.writeRaw8(1 << channel)
 
-def read_sensor():
+def read_sensors():
     try:
         # Initialize the SHT30 sensor
         # Select channel 0
-        tca_select()
+        tca_select(0)
         # Get I2C bus
         bus = smbus.SMBus(1)
         # SHT30 address, 0x44(68)
@@ -69,6 +69,37 @@ def read_sensor():
 
         log.info("Sensor 1 - Temp:{0:0.1f}*C, Temp:{1:0.1f}*F, Humidity: {2:0.1f}%".format(cTemp, fTemp, humidity))
 
+    try:
+        # Initialize the SHT30 sensor
+        # Select channel 1
+        tca_select(1)
+        # Get I2C bus
+        bus = smbus.SMBus(1)
+        # SHT30 address, 0x44(68)
+        # Send measurement command, 0x2C(44)
+        # 0x06(06)        High repeatability measurement
+        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+        # SHT30 address, 0x44(68)
+        # Read data back from 0x00(00), 6 bytes
+        # cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+        data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        # Convert the data
+        cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+        fTemp = cTemp * 1.8 + 32
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+    except RuntimeError as e:
+        # GPIO access may require sudo permissions
+        # Other RuntimeError exceptions may occur, but
+        # are common.  Just try again.
+        log.error("RuntimeError: {}".format(e))
+
+    if humidity is not None and cTemp is not None and fTemp is not None:
+        gh2.set(humidity)
+        gt2.labels('celsius').set(cTemp)
+        gt2.labels('fahrenheit').set(fTemp)
+
+        log.info("Sensor 2 - Temp:{0:0.1f}*C, Temp:{1:0.1f}*F, Humidity: {2:0.1f}%".format(cTemp, fTemp, humidity))
+
     time.sleep(READ_INTERVAL)
 
 if __name__ == "__main__":
@@ -79,4 +110,4 @@ if __name__ == "__main__":
     log.info("Serving sensor metrics on :{}".format(metrics_port))
 
     while True:
-        read_sensor()
+        read_sensors()
