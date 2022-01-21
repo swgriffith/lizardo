cf21adcd8f2b08bc625902ab01dace7a59e577bf
Steve Griffith <steve.griffith@gmail.com>
2022-01-21 17:23:35 +0000

    code cleanup

--- lizardodaemon.py
+++ lizardodaemon.py
@@ -36,11 +36,11 @@ def tca_select(channel):
         return
     tca.writeRaw8(1 << channel)
 
-def read_sensors():
+def read_sensors(channel):
     try:
         # Initialize the SHT30 sensor
         # Select channel 0
-        tca_select(0)
+        tca_select(channel)
         # Get I2C bus
         bus = smbus.SMBus(1)
         # SHT30 address, 0x44(68)
@@ -62,44 +62,11 @@ def read_sensors():
         log.error("RuntimeError: {}".format(e))
 
     if humidity is not None and cTemp is not None and fTemp is not None:
-        gh.labels('sensor1_humidity').set(humidity)
-        gt.labels('sensor1_celsius').set(cTemp)
-        gt.labels('sensor1_fahrenheit').set(fTemp)
+        gh.labels("sensor{}_humidity".format(channel)).set(humidity)
+        gt.labels("sensor{}_celsius".format(channel)).set(cTemp)
+        gt.labels("sensor{}_fahrenheit".format(channel)).set(fTemp)
 
-        log.info("Sensor 1 - Temp:{0:0.1f}*C, Temp:{1:0.1f}*F, Humidity: {2:0.1f}%".format(cTemp, fTemp, humidity))
-
-    try:
-        # Initialize the SHT30 sensor
-        # Select channel 1
-        tca_select(1)
-        # Get I2C bus
-        bus = smbus.SMBus(1)
-        # SHT30 address, 0x44(68)
-        # Send measurement command, 0x2C(44)
-        # 0x06(06)        High repeatability measurement
-        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-        # SHT30 address, 0x44(68)
-        # Read data back from 0x00(00), 6 bytes
-        # cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-        data = bus.read_i2c_block_data(0x44, 0x00, 6)
-        # Convert the data
-        cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
-        fTemp = cTemp * 1.8 + 32
-        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
-    except RuntimeError as e:
-        # GPIO access may require sudo permissions
-        # Other RuntimeError exceptions may occur, but
-        # are common.  Just try again.
-        log.error("RuntimeError: {}".format(e))
-
-    if humidity is not None and cTemp is not None and fTemp is not None:
-        gh.labels('sensor2_humidity').set(humidity)
-        gt.labels('sensor2_celsius').set(cTemp)
-        gt.labels('sensor2_fahrenheit').set(fTemp)
-
-        log.info("Sensor 2 - Temp:{0:0.1f}*C, Temp:{1:0.1f}*F, Humidity: {2:0.1f}%".format(cTemp, fTemp, humidity))
-
-    time.sleep(READ_INTERVAL)
+        log.info("Sensor {0} - Temp:{1:0.1f}*C, Temp:{2:0.1f}*F, Humidity: {3:0.1f}%".format(channel, cTemp, fTemp, humidity))
 
 if __name__ == "__main__":
     # Expose metrics
@@ -109,4 +76,5 @@ if __name__ == "__main__":
     log.info("Serving sensor metrics on :{}".format(metrics_port))
 
     while True:
-        read_sensors()
+        read_sensors(0)
+        read_sensors(1)
